3b1f45d0142ca6688ddb1fee0a4db22166b0203a
Paul Vorbach <paul@vorb.de>
2011-12-27 14:51:19 +0100

    Add functionality

---
+++ pub.js
@@ -0,0 +1,9 @@
+(function() {
+
+module.exports = {
+  add: require('./add.js'),
+  mv:  require('./mv.js'),
+  rm:  require('./rm.js')
+};
+
+}).call(this);
---
+++ rm.js
@@ -0,0 +1,5 @@
+;(function() {
+
+module.exports = null;
+
+}).call(this);
