c73ccb1746a94d4d68b3c87c428fea56681b78a2
Paul Vorbach <paul@vorb.de>
2011-12-30 04:41:09 +0100

    Add functionality

--- pub.js
+++ pub.js
@@ -1,9 +1,39 @@
-(function() {
+// tpl - a general purpose template cli
+// (c) 2011 Paul Vorbach. Licensed under MIT.
+;(function() {
 
-module.exports = {
-  add: require('./add.js'),
-  mv:  require('./mv.js'),
-  rm:  require('./rm.js')
+var fs = require('fs');
+var path = require('path');
+
+var append = require('append');
+var confdir = require('confdir');
+
+module.exports = function (cb) {
+  confdir(process.cwd(), 'pub', function(err, dir) {
+    if (err)
+      cb(err);
+    else
+      cb(null, {
+        version: 'v0.0.1',
+        add: exp('add', dir),
+        mv:  exp('mv', dir),
+        rm:  exp('rm', dir)
+      });
+  });
+};
+
+function exp(cmd, confdir) {
+  return function (files, opt) {
+    var defaultOpt = {
+      cwd: process.cwd()
+    };
+    opt = append(defaultOpt, opt);
+
+    if (typeof files == 'string')
+      files = [ files ];
+
+    require(path.resolve(confdir, cmd + '.js'))(files, opt);
+  };
 };
 
 }).call(this);
